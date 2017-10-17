fb1d48e2966a3bd7700c3618314711d7120b36e9
Sava <vuckovicsava11@gmail.com>
2017-10-17 17:39:02 +0200

    pins can now be created

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const requireAuth = require('../helpers/requireAuth');
+const Pin = require('../models/pin');
 
 /* Welcome page */
 router.get('/', (req, res) => {
@@ -17,6 +18,30 @@ router.get('/home', requireAuth, (req, res) => {
   res.render('home', {
     title: 'Home'
   });
+});
+
+/* Pins */
+router.post('/pins/add', requireAuth, (req, res) => {
+  let allowComments;
+
+  if(req.body.allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  const pin = new Pin({
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: allowComments,
+    author: req.user.id
+  });
+
+  pin.save().then((pin) => {
+    console.log(pin)
+    req.flash('Pin successfully created');
+    res.redirect(`/pins/${pin._id}`)
+  })
 });
 
 module.exports = router;
