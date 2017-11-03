76f3a067b785bd874efd560c533e521518c25d94
Sava <vuckovicsava11@gmail.com>
2017-11-03 13:45:27 +0100

    several minor issues fixed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:'mongodb://localhost/pinterestclone',
-  googleClientID:'983869381010-o9a69qgf0vf7fms39f032s0k6rku51vg.apps.googleusercontent.com',
-  googleClientSecret:'saiy82kC5mfJ_Injo1EpAb2j',
+  googleClientID:'193233367705-5429l689hb3uhbtgnofksda692s6p06m.apps.googleusercontent.com',
+  googleClientSecret:'KmGtLjNGgRgZE_75EHnbhkSa',
   sessionSecret: 'TheSecRetK3y'
 }
