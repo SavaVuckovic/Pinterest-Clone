2b2ff690e40f73a88ee62dfea811ecc1dfd4bc66
Sava <vuckovicsava11@gmail.com>
2017-11-03 12:43:53 +0100

    application prepared for deployment on heroku

--- README.md
+++ README.md
@@ -5,12 +5,6 @@ TODOS:
 
 - implement twitter OAuth
 
-- improve logic for CRUD
-
 - improve authentication logic
-
-- test everything, try to break the app
-
-- deploy to Heroku
 
 - write a README
---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:'mongodb://localhost/pinterestclone',
+  googleClientID:'983869381010-o9a69qgf0vf7fms39f032s0k6rku51vg.apps.googleusercontent.com',
+  googleClientSecret:'saiy82kC5mfJ_Injo1EpAb2j',
+  sessionSecret: 'TheSecRetK3y'
+}
